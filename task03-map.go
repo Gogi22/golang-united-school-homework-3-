0c825fbe2aa1ead3b65ff25bbd06fdb568315397
Gogi <Gogi22@users.noreply.github.com>
2022-05-08 15:29:00 +0400

    finished ds tasks

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,19 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	digits := make([]int, 0, len(input))
+
+	for k := range input {
+		digits = append(digits, k)
+	}
+
+	sort.Ints(digits)
+
+	for _, k := range digits {
+		result = append(result, input[k])
+	}
+
 	return
 }
